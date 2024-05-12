e212f0ba5995fa9ebc43c40e6b1ac626bdd983d7
VACHAN SHETTY <vachan316@gmail.com>
2024-05-12 20:29:47 +0530

    fixed my-location hidden issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -132,7 +132,8 @@ export default function Navbar({ location }: { location?: string }) {
             <MdMyLocation
               title="Your Current Location"
               onClick={handleCurrentLocation}
-              className="cursor:pointer text-2xl relative inline-flex h-fit w-fit rounded-full border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-opacity-15 focus:ring-2 md:hidden"
+              className="cursor:pointer text-2xl relative inline-flex h-fit w-fit rounded-full border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-opacity-15 focus:ring-2
+              "
             />
             <MdOutlineLocationOn className="text-3xl text-white hidden md:block" />
             <p className="gradient-text text-sm hidden md:block">
